6b0ca7e1eda51b04260b8376b239dced21598e7f
sethcj1 <160380349+sethcj1@users.noreply.github.com>
2024-04-10 12:51:43 -0400

    Add files via upload

---
+++ Lab 9.py
@@ -0,0 +1,33 @@
+menu = ("Menu\n"
+        "-------------\n"
+        "1. Encode\n"
+        "2. Decode\n"
+        "3. Quit\n")
+
+
+inmenu = True
+
+def encode(string):
+    encoded = ""
+    for i in string:
+        character = int(i) +3
+        encoded += str(character)
+    return encoded
+
+def decode(string):
+    decoded = ""
+    for i in string:
+        character = int(i) -3
+        decoded += str(character)
+    return decoded
+
+while inmenu:
+    print(menu)
+    option = input("Please enter an option: ")
+    if option == "1":
+        password = str(input("Please enter your password to encode: "))
+        print("Your password has been encoded and stored!")
+    elif option == "2":
+        print(f"The encoded password is {encode(password)} and the original password is {password}")
+    elif option == "3":
+        exit()
